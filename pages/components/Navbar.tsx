ecb274b76d86b2c013312c9ef8a646138894d61c
Kabilan <kumarakabilan2004@gmail.com>
2023-10-15 00:31:36 +0530

    Navbar Link

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,14 +1,17 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
 import { ConnectWallet} from "@thirdweb-dev/react";
 import TokenBal from './TokenBal';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <Box bgColor="transparent" p={4} position="fixed" top={0} left={0} right={0} display="flex" justifyContent="space-between" alignItems="center" >
       <Flex align="center">
+      <Link href="/">
         <Text fontSize="xl" fontWeight="bold" color="white">
           WorldsBest
         </Text>
+      </Link>
     </Flex>
       <Flex>
         <Box mr={4}>
