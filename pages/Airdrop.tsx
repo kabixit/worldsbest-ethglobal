7ae0b377e30caf52dfcb909b85455a9a1b9c93ee
Kabilan <kumarakabilan2004@gmail.com>
2023-10-16 01:02:24 +0530

    airdrop done

--- pages/Airdrop.tsx
+++ pages/Airdrop.tsx
@@ -9,36 +9,61 @@ import {
   Text,
 } from '@chakra-ui/react';
 import Navbar from './components/Navbar';
-import { useAddress, useContract, useContractWrite } from '@thirdweb-dev/react';
+import { useContract, useContractWrite, useAddress } from '@thirdweb-dev/react';
 
-const AirdropPage = () => {
-  const { contract } = useContract("0x288319e58019460A6DA33F52F778a88B5BC18DaC");
+export default function AirdropPage() {
+  const contractAddress = "0xe130669C544076bE32dEA26c11f0e73D7200f7aF";
+  const { contract } = useContract(contractAddress);
   const account = useAddress();
-  const { mutateAsync: multicall, isLoading } = useContractWrite(contract, "multicall");
+  const { mutateAsync: airdrop, isLoading } = useContractWrite(contract, "airdrop");
 
-  const recipients = ["0x60117B4da2C71B092357CCc035F8C56f55b69538", "0x442Bb05C11e44BB7e6aE5cb34Bbaf46b0f7dDA88"];
-  const amounts = [100, 200];// Amount of tokens to airdrop
+  const [recipientAddresses, setRecipientAddresses] = useState("");
+  const [amounts, setAmounts] = useState("");
 
-  const inputData = JSON.stringify([recipients, amounts]);
+  const handleRecipientAddressesChange = (event) => {
+    setRecipientAddresses(event.target.value);
+  };
+
+  const handleAmountsChange = (event) => {
+    setAmounts(event.target.value);
+  };
 
   const handleAirdrop = async () => {
+    // Split comma-separated addresses and amounts into arrays
+    const recipientAddressesArray = recipientAddresses.split(',');
+    const amountsArray = amounts.split(',');
+    
+
+
     try {
-      const data = await multicall({ args: [inputData] });
-      console.info("Airdrop success", data);
+      // Ensure that the number of recipients matches the number of amounts
+      if (recipientAddressesArray.length !== amountsArray.length) {
+        console.error("Number of recipients must match the number of amounts");
+        return;
+      }
+
+      // Convert addresses and amounts to the required format
+      const _tokenAddress = "0x288319e58019460A6DA33F52F778a88B5BC18DaC";
+      const _tokenOwner = account;
+      const _recipients = recipientAddressesArray;
+      const _amounts = amountsArray.map(amount => String(parseInt(amount) * 1e18));
+
+      const data = await airdrop({ args: [_tokenAddress, _tokenOwner, _recipients, _amounts] });
+      console.info("Contract call success", data);
     } catch (err) {
-        console.error("Airdrop failed", err);
+      console.error("Contract call failure", err);
     }
-  }
+  };
 
   return (
-    <>
+    <ChakraProvider>
       <Navbar />
       <Flex
         direction="column"
         align="center"
         h="100vh"
-        bgColor="blackAndWhite.900"
-        color="blackAndWhite.50"
+        bgColor="black"
+        color="white"
         animation="fadeIn 1s ease-in-out"
         justify="center"
       >
@@ -53,17 +78,21 @@ const AirdropPage = () => {
             type="text"
             placeholder="Recipient Addresses (comma-separated)"
             size="lg"
+            value={recipientAddresses}
+            onChange={handleRecipientAddressesChange}
           />
         </Box>
         <Box mb={6}>
           <Input
-            type="number"
-            placeholder="Amount"
+            type="text"
+            placeholder="Amounts (comma-separated)"
             size="lg"
+            value={amounts}
+            onChange={handleAmountsChange}
           />
         </Box>
         <Button
-          colorScheme="brand"
+          colorScheme="blue"
           size="lg"
           onClick={handleAirdrop}
           isLoading={isLoading}
@@ -71,8 +100,6 @@ const AirdropPage = () => {
           Airdrop
         </Button>
       </Flex>
-    </>
+    </ChakraProvider>
   );
-};
-
-export default AirdropPage;
+}
